d69876b38f70b64bb3862071918110997edcf132
thanhvp <phuocthanh20cgh@gmail.com>
2023-11-23 22:21:35 +0700

    display fullname of tech in schedule

--- src/scenesManager/calendarTechnician/index.jsx
+++ src/scenesManager/calendarTechnician/index.jsx
@@ -29,6 +29,8 @@ const CalendarTechnician = () => {
   const [id, setId] = useState("");
   const theme = createTheme();
   const colors = tokens(theme.palette.mode);
+  const [technicianId, setTechnicianId] = useState("");
+  const [technicianData, setTechnicianData] = useState({});
 
   const RESOURCES = [
     { id: "a", title: "Auditorium A" },
@@ -39,71 +41,151 @@ const CalendarTechnician = () => {
   function createEventId() {
     return String(eventGuid++);
   }
+  // const fetchShiftDataForWeek = async (startDateStr) => {
+  //   const days = getWeekDays(startDateStr);
+  //   const promises = days.map((day) =>
+  //     dispatch(getShiftOfDate({ date: day })).unwrap()
+  //   );
+  //   try {
+  //     const responses = await Promise.all(promises);
+  //     console.log(responses);
+  //     const updatedEvents = [];
+
+  //     responses.forEach((response) => {
+  //       if (response && response.data && response.data.length > 0) {
+  //         const eventData = response.data[0];
+
+  //         const eventId = createEventId();
+  //         console.log(eventData.type);
+  //         console.log(eventData.date);
+  //         // Tạo sự kiện từ dữ liệu nhận được từ API
+
+  //         let startHour = "00:00:00";
+  //         let endHour = "08:00:00";
+
+  //         if (eventData.type === "Midnight") {
+  //           startHour = "00:00:00";
+  //           endHour = "08:00:00";
+  //         } else if (eventData.type === "Night") {
+  //           startHour = "16:00:00";
+  //           endHour = "24:00:00"; // Kết thúc vào ngày tiếp theo
+  //         } else if (eventData.type === "Morning") {
+  //           startHour = "08:00:00";
+  //           endHour = "16:00:00";
+  //         } else {
+  //           // Xử lý các trường hợp khác của eventData.type ở đây (nếu cần)
+  //         }
+
+  //         const event = {
+  //           id: eventId,
+  //           title: eventData.type,
+  //           description: eventData.technicianId,
+  //           start: eventData.date.substring(0, 10) + "T" + startHour,
+  //           end: eventData.date.substring(0, 10) + "T" + endHour,
+  //           // Các trường thông tin khác từ API có thể được thêm vào đây
+  //         };
+  //         // Lưu giá trị technicianId vào state khi có dữ liệu eventData.technicianId
+  //         if (eventData.technicianId) {
+  //           const technicianId = eventData.technicianId;
+  //           setTechnicianId(technicianId);
+
+  //           // Gọi hàm fetchFullNameTechnician để lấy fullname từ API
+  //           fetchFullNameTechnician(technicianId);
+  //         }
+  //         updatedEvents.push(event);
+  //       }
+  //     });
+
+  //     setInitialEvents(updatedEvents);
+  //   } catch (error) {
+  //     console.error("Error fetching data for the week:", error);
+  //   }
+  // };
+
   const fetchShiftDataForWeek = async (startDateStr) => {
     const days = getWeekDays(startDateStr);
     const promises = days.map((day) => 
       dispatch(getShiftOfDate({ date: day })).unwrap()
     );
+  
     try {
       const responses = await Promise.all(promises);
-        console.log(responses);
-        const updatedEvents = [];
+      const eventPromises = responses.map(async (response) => {
+        if (response && response.data && response.data.length > 0) {
+          const eventData = response.data[0];
+          const fullname = await fetchFullNameTechnician(eventData.technicianId);
+          console.log(fullname);
+          
+          // Tạo sự kiện từ dữ liệu nhận được từ API
+        
+          let startHour = "00:00:00";
+          let endHour = "08:00:00";
 
-        responses.forEach((response) => {
-          if (response && response.data && response.data.length > 0) {
-            const eventData = response.data[0];
-
-            const eventId = createEventId();
-            console.log(eventData.type);
-            console.log(eventData.date);
-            // Tạo sự kiện từ dữ liệu nhận được từ API
-
-            let startHour = "00:00:00";
-            let endHour = "08:00:00";
-
-            if (eventData.type === "Midnight") {
-              startHour = "00:00:00";
-              endHour = "08:00:00";
-            } else if (eventData.type === "Night") {
-              startHour = "16:00:00";
-              endHour = "24:00:00"; // Kết thúc vào ngày tiếp theo
-            } else if (eventData.type === "Morning") {
-              startHour = "08:00:00";
-              endHour = "16:00:00";
-            } else {
-              // Xử lý các trường hợp khác của eventData.type ở đây (nếu cần)
-            }
-              
-            const event = {
-              id: eventId,
-              title: eventData.type,
-              description: eventData.technicianId,
-              start: eventData.date.substring(0, 10) + "T" + startHour,
-              end: eventData.date.substring(0, 10) + "T" + endHour,
-              // Các trường thông tin khác từ API có thể được thêm vào đây
-            };
-
-            updatedEvents.push(event);
-            
+          if (eventData.type === "Midnight") {
+            startHour = "00:00:00";
+            endHour = "08:00:00";
+          } else if (eventData.type === "Night") {
+            startHour = "16:00:00";
+            endHour = "24:00:00"; // Kết thúc vào ngày tiếp theo
+          } else if (eventData.type === "Morning") {
+            startHour = "08:00:00";
+            endHour = "16:00:00";
+          } else {
+            // Xử lý các trường hợp khác của eventData.type ở đây (nếu cần)
           }
-        });
-
-        setInitialEvents(updatedEvents);
+  
+          return {
+            id: createEventId(),
+            title: eventData.type,
+            description:fullname, // Đặt fullname vào trường description
+            start: eventData.date.substring(0, 10) + "T" + startHour,
+            end: eventData.date.substring(0, 10) + "T" + endHour,
+          };
+        }
+        return null;
+      });
+  
+      const updatedEvents = (await Promise.all(eventPromises)).filter(event => event);
+      setInitialEvents(updatedEvents);
     } catch (error) {
       console.error("Error fetching data for the week:", error);
     }
   };
   const fetchFullNameTechnician = async (technicianId) => {
     try {
-      // Gọi API để lấy thông tin fullname
       const response = await dispatch(getTechnicianId({ id: technicianId })).unwrap();
-      return response.fullname;
+      console.log("Response data:", response.data); // Log to check the structure of response data
+  
+      // Check the structure of response data to find the correct field containing fullname
+      const fullname = response.data?.fullname || response.data?.name ;
+      return fullname;
     } catch (error) {
       console.error("Error fetching fullname:", error);
       return null;
     }
   };
   
+  
+// useEffect để theo dõi thay đổi của technicianData và cập nhật description của event
+useEffect(() => {
+  // Nếu có technicianData và fullname được trả về từ API
+  if (technicianData && technicianData.fullname) {
+    console.log(technicianData.fullname)
+    // Cập nhật description của event với fullname từ technicianData
+    setInitialEvents((prevEvents) => {
+      return prevEvents.map((event) => {
+        if (event.description === technicianData.technicianId) {
+          return {
+            ...event,
+            description: technicianData.fullname,
+          };
+        }
+        return event;
+      });
+    });
+  }
+}, [technicianData]);
+
   useEffect(() => {
     const getShiftData = async () => {
       try {
@@ -160,7 +242,7 @@ const CalendarTechnician = () => {
 
     getShiftData();
   }, [dispatch]);
-  
+
   function getMondayOfWeek(date) {
     const day = date.getDay() || 7; // Chuyển đổi Chủ nhật từ 0 thành 7
     if (day !== 0) {
@@ -186,7 +268,7 @@ const CalendarTechnician = () => {
       dispatch(getShiftOfDate({ date: selectInfo.startStr }));
     }
   };
- 
+
   // Hàm xử lý khi chọn ngày trên lịch
   const handleDatesSet = (info) => {
     fetchShiftDataForWeek(info.startStr);
@@ -278,7 +360,7 @@ const CalendarTechnician = () => {
             center: "title",
             right: "timeGridWeek,timeGridDay,listMonth",
           }}
-          initialView="dayGridMonth"
+          initialView="timeGridWeek"
           editable={true}
           selectable={true}
           selectMirror={true}
