1491a3029ad521f95f29ce218b85b2918e228d71
thanhvp <phuocthanh20cgh@gmail.com>
2023-11-26 13:13:05 +0700

    revices push notification

--- public/firebase.messaging.sw.js
+++ public/firebase.messaging.sw.js
@@ -1,35 +1,44 @@
 /* eslint-disable no-undef */
+
+
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { useDispatch } from 'react-redux';
+import { sendNotification } from "../src/redux/orderSlice";
+import { getMessaging,onMessage,getToken } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
-
-const firebaseConfig = {  
-    apiKey: "AIzaSyDwwpps4RBQJUWK17ivjFZTSl5Gx5zP7s8",
-authDomain: "car-rescue-399511.firebaseapp.com",
-projectId: "car-rescue-399511",
-storageBucket: "car-rescue-399511.appspot.com",
-messagingSenderId: "387575068560",
-appId: "1:387575068560:web:52c7c2e4f8684b230a2515",
-measurementId: "G-SYYVJXV5HQ"
+const firebaseConfig = {
+  apiKey: "AIzaSyDwwpps4RBQJUWK17ivjFZTSl5Gx5zP7s8",
+  authDomain: "car-rescue-399511.firebaseapp.com",
+  projectId: "car-rescue-399511",
+  storageBucket: "car-rescue-399511.appspot.com",
+  messagingSenderId: "387575068560",
+  appId: "1:387575068560:web:52c7c2e4f8684b230a2515",
+  measurementId: "G-SYYVJXV5HQ"
 };
 
-// eslint-disable-next-line no-undef
-firebase.initializeApp(firebaseConfig);
-// eslint-disable-next-line no-undef
-const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification.title;
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+const messaging = getMessaging(app);
+
+
+messaging.onBackgroundMessage(messaging, (payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
   };
 
   // eslint-disable-next-line no-restricted-globals
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 
 import { initializeApp } from "firebase/app";
 import firebase from "firebase/app";
@@ -5,6 +7,7 @@ import { getStorage } from "firebase/storage";
 import { useDispatch } from 'react-redux';
 import { sendNotification } from "../redux/orderSlice";
 import { getMessaging,onMessage,getToken } from "firebase/messaging";
+import { onBackgroundMessage } from "firebase/messaging/sw";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwwpps4RBQJUWK17ivjFZTSl5Gx5zP7s8",
@@ -52,102 +55,17 @@ export const onMessageListener = (callback) =>
         callback(payload);
     });
 });
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
-
-// export function requestPermissions() {
-//   if ("Notification" in window && "requestPermission" in window.Notification) {
-//     console.log("Requesting permissions");
-//     Notification.requestPermission()
-//       .then((permission) => {
-//         if (permission === "granted") {
-//           console.log("Permission granted");
-
-//           getToken(messaging, { vapidKey: "BEFk_eArFD9yGN4dUQ3G2NVqddX2sObRGxkKV1deH96RJawVKIgVMm1-h_jYRC2Vjy-5y3XaE1ZP7xOaXIv-G1Q" })
-//             .then((currentToken) => {
-//               if (currentToken) {
-//                 console.log("Current token for client:", currentToken);
-//                 localStorage.setItem('deviceToken', currentToken);
-//                 // Perform any other necessary action with the token
-//               } else {
-//                 console.log("No registration token available. Request permission to generate one.");
-//               }
-//             })
-//             .catch((err) => {
-//               console.error("An error occurred while retrieving token:", err);
-//             });
-//         } else {
-//           console.log("Permission denied");
-//         }
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js')
+//       .then(registration => {
+//         console.log('SW registered: ', registration);
 //       })
-//       .catch((error) => {
-//         console.error("Permission request failed:", error);
+//       .catch(registrationError => {
+//         console.log('SW registration failed: ', registrationError);
 //       });
-//   } else {
-//     console.log("Notification API not supported");
-//   }
-// }
-
-
-// export const getMessagingToken = async () => {
-//   let currentToken = "";
-//   if (!messaging) return;
-//   try {
-//     currentToken = await messaging.getToken({
-//       vapidKey: process.env.REACT_APP_FIREBASE_FCM_VAPID_KEY,
-//     });
-//     console.log("FCM registration token", currentToken);
-//   } catch (error) {
-//     console.log("An error occurred while retrieving token. ", error);
-//   }
-//   return currentToken;
-// };
-// export const fetchToken = (setTokenFound) => {
-//   return getToken(messaging, {vapidKey: 'BHGPr3pJQSflJAJtTIVXbmcEXlPV_HP29TZQRcqrGCN10gKIa-ojIJmtvM9kQGcsNKsWIA6ezKFG8Bd6LTjaVc0'}).then((currentToken) => {
-//     if (currentToken) {
-//       console.log('current token for client: ', currentToken);
-//       setTokenFound(true);
-//       // Track the token -> client mapping, by sending to backend server
-//       // show on the UI that permission is secured
-//     } else {
-//       console.log('No registration token available. Request permission to generate one.');
-//       setTokenFound(false);
-//       // shows on the UI that permission is required 
-//     }
-//   }).catch((err) => {
-//     console.log('An error occurred while retrieving token. ', err);
-//     // catch error while creating client token
 //   });
 // }
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/firebase-messaging-sw.js')
-//     .then((registration) => {
-//       console.log("Service Worker registered with scope:", registration.scope);
-//     })
-//     .catch((error) => {
-//       console.error("Service Worker registration failed:", error);
-//     });
-// }
-
-// const messaging = getMessaging(app)
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker.register("/firebase-messaging-sw.js") // Update the path accordingly
-//     .then((registration) => {
-//       console.log("Service Worker registered with scope:", registration.scope);
-//     })
-//     .catch((error) => {
-//       console.error("Service Worker registration failed:", error);
-//     });
-// }
 
 
