532a28d65556d75d5c794426b2b21938126c9d3e
thanhvp <phuocthanh20cgh@gmail.com>
2023-12-03 16:02:01 +0700

    add notification use localstorage

--- src/features/auth/RequireAuth.js
+++ src/features/auth/RequireAuth.js
@@ -1,14 +1,80 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import {
-
-  selectCurrentUser,
-} from "./authSlice";
+import { selectCurrentUser } from "./authSlice";
 import jwt_decode from "jwt-decode";
 import ManagerContent from "./Manager";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { onMessageListener } from "../../firebase";
+import Topbar from "../../components/Topbar";
 const RequireAuth = () => {
+  const MAX_NOTIFICATIONS_DISPLAYED = 5;
+  const [unreadNotifications, setUnreadNotifications] = useState(0);
 
+  const saveNotificationToLocalStorage = (payload) => {
+    const now = new Date();
   
+    const notification = {
+      title: payload.notification.title,
+      body: payload.notification.body,
+      image: payload.notification.image,
+      receivedTime: now.toISOString(),
+    };
+  
+    // Lấy danh sách thông báo từ localStorage nếu có
+    const notificationsFromLocalStorage = localStorage.getItem("notifications");
+    const notifications = notificationsFromLocalStorage
+      ? JSON.parse(notificationsFromLocalStorage)
+      : [];
+  
+    // Thêm thông báo mới vào đầu danh sách
+    notifications.unshift(notification);
+  
+    // Giới hạn số lượng thông báo
+    const limitedNotifications = notifications.slice(
+      0,
+      MAX_NOTIFICATIONS_DISPLAYED
+    );
+  
+    // Tăng số lượng thông báo chưa đọc lên 1
+    setUnreadNotifications((prevUnreadCount) => prevUnreadCount + 1);
+  };
+  
+  useEffect(() => {
+    localStorage.setItem(
+      "unreadNotificationsCount",
+      unreadNotifications.toString()
+    );
+  }, [unreadNotifications]);
+  
+  
+  
+
+  useEffect(() => {
+    const handleMessage = (payload) => {
+      toast(
+        `Title: ${payload.notification.title}, Body: ${payload.notification.body}`,
+        {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        }
+      );
+      saveNotificationToLocalStorage(payload);
+      return {
+        unreadNotifications,
+        setUnreadNotifications,
+        saveNotificationToLocalStorage,
+      };
+    };
+
+    onMessageListener(handleMessage);
+  }, []);
+
   // Sử dụng useSelector để truy cập giá trị token từ Redux store
   const data = useSelector(selectCurrentUser);
   if (data) {
@@ -41,9 +107,9 @@ const RequireAuth = () => {
   const devicetoken1 = localStorage.getItem("deviceToken");
   if (devicetoken1) {
     // Có deviceToken, bạn có thể sử dụng nó tại đây
-    console.log("Test device"+devicetoken1);
+    console.log("Test device" + devicetoken1);
   }
-  
+
   const thisrole = data?.role ? `Role ${data?.role}!` : "role!";
   // Sử dụng useLocation để lấy thông tin vị trí hiện tại (đường dẫn URL) của ứng dụng
   const location = useLocation();
