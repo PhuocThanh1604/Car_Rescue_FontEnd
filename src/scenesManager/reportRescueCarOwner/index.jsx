4027f9bc7f00e1ca43414cc6a08a1e684b7dd2d9
thanhvp <phuocthanh20cgh@gmail.com>
2023-12-10 04:30:29 +0700

    update

--- src/scenesManager/reportRescueCarOwner/index.jsx
+++ src/scenesManager/reportRescueCarOwner/index.jsx
@@ -221,15 +221,7 @@ const Reports = (props) => {
     console.log(vehicleId);
     setIsAccepted(accept);
     console.log(accept);
-    const messageAccept = {
-      title: "Hệ thống đã ghi nhận báo cáo đơn hàng của bạn!!",
-      body: "Hệ thống sẽ phản hồi đến quí khách thời gian sớm nhất",
-    };
-    const messageRejected = {
-      title: "Hệ thống Không chấp nhận đơn báo cáo đơn hàng của bạn!!",
-      body: "Xin lỗi!! Bạn báo cáo của bạn không đủ điều kiện!!",
-    };
-    // Fetch the VehicleId details based on the selected Vehicle ID
+ 
     dispatch(acceptReport({ id: vehicleId, boolean: accept }))
       .then(() => {
         const updatedVehicles = filteredVehicles.filter(
@@ -241,44 +233,9 @@ const Reports = (props) => {
         setIsSuccess(true);
         if (accept) {
           toast.success("Chấp nhận đơn báo cáo thành công");
-          const notificationData = {
-            deviceId: "",
-            isAndroiodDevice: true,
-            title: messageAccept.title,
-            body: messageAccept.body,
-          };
-
-          // Gửi thông báo bằng hàm sendNotification
-          dispatch(sendNotification(notificationData))
-            .then((res) => {
-              if (res.payload.message === "Notification sent successfully")
-                toast.success("Gửi thông báo thành công");
-              console.log("Gửi thông báo thành công");
-            })
-            .catch((error) => {
-              toast.error("Gửi thông không thành công vui lòng thử lại!!");
-              console.error("Lỗi khi gửi thông báo:", error);
-            });
+     
         } else {
           toast.error("Không đồng chấp nhận xe vào hệ thống ");
-          const notificationData = {
-            deviceId: "",
-            isAndroiodDevice: true,
-            title: messageRejected.title,
-            body: messageRejected.body,
-          };
-
-          // Gửi thông báo bằng hàm sendNotification
-          dispatch(sendNotification(notificationData))
-            .then((res) => {
-              if (res.payload.message === "Notification sent successfully")
-                toast.success("Gửi thông báo đến khách hàng thành công");
-              console.log("Gửi thông báo thành công");
-            })
-            .catch((error) => {
-              toast.error("Gửi thông báo đến khách hàng không thành công!!");
-              console.error("Lỗi khi gửi thông báo:", error);
-            });
         }
       })
       .catch((error) => {
